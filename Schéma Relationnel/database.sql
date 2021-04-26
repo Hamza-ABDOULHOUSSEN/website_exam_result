e2a3da0e3c02e80f7659242aba511c1ddd33cdfc
Lucas Gouin <Lucas.Gouin@telecomnancy.eu>
2021-04-26 09:42:38 +0200

    conversion commande en sqlite

--- Schéma Relationnel/database.sql
+++ Schéma Relationnel/database.sql
@@ -1,285 +1,285 @@
 CREATE TABLE Candidat(
-    candidat_id   int PRIMARY KEY,
-    Civ           text NOT NULL,
-    Nom           text NOT NULL,
-    Prenom        text NOT NULL,
-    Naissance     text NOT NULL,
-    Adresse1      text NOT NULL,
-    Adresse2      text NOT NULL,
-    Code_Postal   int  NOT NULL,
-    Commune       text NOT NULL,
-    Pays          text NOT NULL,
-    Email         text NOT NULL,
-    Telephone     text,
-    Portable      text,
-    Filliere      text NOT NULL,
-    Statut        text,
-    Etablissement text
+    candidat_id   INTEGER PRIMARY KEY,
+    Civ           TEXT NOT NULL,
+    Nom           TEXT NOT NULL,
+    Prenom        TEXT NOT NULL,
+    Naissance     TEXT NOT NULL,
+    Adresse1      TEXT NOT NULL,
+    Adresse2      TEXT NOT NULL,
+    Code_Postal   INTEGER  NOT NULL,
+    Commune       TEXT NOT NULL,
+    Pays          TEXT NOT NULL,
+    Email         TEXT NOT NULL,
+    Telephone     TEXT,
+    Portable      TEXT,
+    Filliere      TEXT NOT NULL,
+    Statut        TEXT,
+    Etablissement TEXT
 );
 
 CREATE TABLE Classement_Note_MP(
-    candidat_id  int PRIMARY KEY,
-    Math1        double,
-    Math2        double,
-    Phy1         double,
-    Phy2         double,
-    Chimie       double,
-    Fr           double,
-    LV1          double,
-    IPT          double,
-    Spe          double,
-    bonification int,
-    total_ecrit  double,
-    rang_ecrit   int,
-    Math_oral    double,
-    Phy_oral     double,
-    Fr_oral      double,
-    Ang_oral     double,
-    total_oral   double,
-    total        double,
-    rang         int,
+    candidat_id  INTEGER PRIMARY KEY,
+    Math1        DOUBLE,
+    Math2        DOUBLE,
+    Phy1         DOUBLE,
+    Phy2         DOUBLE,
+    Chimie       DOUBLE,
+    Fr           DOUBLE,
+    LV1          DOUBLE,
+    IPT          DOUBLE,
+    Spe          DOUBLE,
+    bonification INTEGER,
+    total_ecrit  DOUBLE,
+    rang_ecrit   INTEGER,
+    Math_oral    DOUBLE,
+    Phy_oral     DOUBLE,
+    Fr_oral      DOUBLE,
+    Ang_oral     DOUBLE,
+    total_oral   DOUBLE,
+    total        DOUBLE,
+    rang         INTEGER,
     FOREIGN KEY (candidat_id) REFERENCES Candidat (candidat_id)
 );
 
 CREATE TABLE Classement_Note_PSI(
-    candidat_id  int PRIMARY KEY,
-    Math1        double,
-    Math2        double,
-    Phy1         double,
-    Phy2         double,
-    Chimie       double,
-    Fr           double,
-    LV1          double,
-    IPT          double,
-    SI           double,
-    bonification int,
-    total_ecrit  double,
-    rang_ecrit   int,
-    Math_oral    double,
-    Phy_oral     double,
-    Fr_oral      double,
-    Ang_oral     double,
-    total_oral   double,
-    total        double,
-    rang         int,
+    candidat_id  INTEGER PRIMARY KEY,
+    Math1        DOUBLE,
+    Math2        DOUBLE,
+    Phy1         DOUBLE,
+    Phy2         DOUBLE,
+    Chimie       DOUBLE,
+    Fr           DOUBLE,
+    LV1          DOUBLE,
+    IPT          DOUBLE,
+    SI           DOUBLE,
+    bonification INTEGER,
+    total_ecrit  DOUBLE,
+    rang_ecrit   INTEGER,
+    Math_oral    DOUBLE,
+    Phy_oral     DOUBLE,
+    Fr_oral      DOUBLE,
+    Ang_oral     DOUBLE,
+    total_oral   DOUBLE,
+    total        DOUBLE,
+    rang         INTEGER,
     FOREIGN KEY (candidat_id) REFERENCES Candidat (candidat_id)
 );
 
 CREATE TABLE Classement_Note_PC(
-    candidat_id  int PRIMARY KEY,
-    Math1        double,
-    Math2        double,
-    Phy1         double,
-    Phy2         double,
-    Chimie       double,
-    Fr           double,
-    LV1          double,
-    IPT          double,
-    bonification int,
-    total_ecrit  double,
-    rang_ecrit   int,
-    Math_oral    double,
-    Phy_oral     double,
-    Fr_oral      double,
-    Ang_oral     double,
-    total_oral   double,
-    total        double,
-    rang         int,
+    candidat_id  INTEGER PRIMARY KEY,
+    Math1        DOUBLE,
+    Math2        DOUBLE,
+    Phy1         DOUBLE,
+    Phy2         DOUBLE,
+    Chimie       DOUBLE,
+    Fr           DOUBLE,
+    LV1          DOUBLE,
+    IPT          DOUBLE,
+    bonification INTEGER,
+    total_ecrit  DOUBLE,
+    rang_ecrit   INTEGER,
+    Math_oral    DOUBLE,
+    Phy_oral     DOUBLE,
+    Fr_oral      DOUBLE,
+    Ang_oral     DOUBLE,
+    total_oral   DOUBLE,
+    total        DOUBLE,
+    rang         INTEGER,
     FOREIGN KEY (candidat_id) REFERENCES Candidat (candidat_id)
 );
 
 CREATE TABLE Classement_Note_PT(
-    candidat_id  int PRIMARY KEY,
-    Math1        double,
-    Math2        double,
-    Phy1         double,
-    Phy2         double,
-    Info_Model   double,
-    SI           double,
-    Fr           double,
-    LV1          double,
-    bonification int,
-    total_ecrit  double,
-    rang_ecrit   int,
-    Math_oral    double,
-    Phy_oral     double,
-    Fr_oral      double,
-    Ang_oral     double,
-    total_oral   double,
-    total        double,
-    rang         int,
+    candidat_id  INTEGER PRIMARY KEY,
+    Math1        DOUBLE,
+    Math2        DOUBLE,
+    Phy1         DOUBLE,
+    Phy2         DOUBLE,
+    Info_Model   DOUBLE,
+    SI           DOUBLE,
+    Fr           DOUBLE,
+    LV1          DOUBLE,
+    bonification INTEGER,
+    total_ecrit  DOUBLE,
+    rang_ecrit   INTEGER,
+    Math_oral    DOUBLE,
+    Phy_oral     DOUBLE,
+    Fr_oral      DOUBLE,
+    Ang_oral     DOUBLE,
+    total_oral   DOUBLE,
+    total        DOUBLE,
+    rang         INTEGER,
     FOREIGN KEY (candidat_id) REFERENCES Candidat (candidat_id)
 );
 
 CREATE TABLE Classement_Note_TSI(
-    candidat_id  int PRIMARY KEY,
-    Math1        double,
-    Math2        double,
-    Phy1         double,
-    Phy2         double,
-    Fr           double,
-    LV1          double,
-    SI           double,
-    Info         double,
-    bonification int,
-    total_ecrit  double,
-    rang_ecrit   int,
-    Math1_oral   double,
-    Math2_oral   double,
-    Phy1_oral    double,
-    Phy2_oral    double,
-    LV_oral      double,
-    TP_Phy_oral  double,
-    S2I_oral     double,
-    total_oral   double,
-    total        double,
-    rang         int,
+    candidat_id  INTEGER PRIMARY KEY,
+    Math1        DOUBLE,
+    Math2        DOUBLE,
+    Phy1         DOUBLE,
+    Phy2         DOUBLE,
+    Fr           DOUBLE,
+    LV1          DOUBLE,
+    SI           DOUBLE,
+    Info         DOUBLE,
+    bonification INTEGER,
+    total_ecrit  DOUBLE,
+    rang_ecrit   INTEGER,
+    Math1_oral   DOUBLE,
+    Math2_oral   DOUBLE,
+    Phy1_oral    DOUBLE,
+    Phy2_oral    DOUBLE,
+    LV_oral      DOUBLE,
+    TP_Phy_oral  DOUBLE,
+    S2I_oral     DOUBLE,
+    total_oral   DOUBLE,
+    total        DOUBLE,
+    rang         INTEGER,
     FOREIGN KEY (candidat_id) REFERENCES Candidat (candidat_id)
 );
 
 CREATE TABLE Classement_Note_ATS(
-    candidat_id      int PRIMARY KEY,
-    Math             double,
-    Phy              double,
-    Fr               double,
-    Ang              double,
-    SI               double,
-    bonification     int,
-    moyenne_ecrit    double,
-    total_ecrit      double,
-    rang_ecrit       int,
-    Math_oral        double,
-    Phy_oral         double,
-    Genie_elec_oral  double,
-    Genie_meca_oral  double,
-    LV_oral          double,
-    Epreuve_ATS_oral double,
-    moyenne_oral     double,
-    total_oral       double,
-    rang_oral        int,
+    candidat_id      INTEGER PRIMARY KEY,
+    Math             DOUBLE,
+    Phy              DOUBLE,
+    Fr               DOUBLE,
+    Ang              DOUBLE,
+    SI               DOUBLE,
+    bonification     INTEGER,
+    moyenne_ecrit    DOUBLE,
+    total_ecrit      DOUBLE,
+    rang_ecrit       INTEGER,
+    Math_oral        DOUBLE,
+    Phy_oral         DOUBLE,
+    Genie_elec_oral  DOUBLE,
+    Genie_meca_oral  DOUBLE,
+    LV_oral          DOUBLE,
+    Epreuve_ATS_oral DOUBLE,
+    moyenne_oral     DOUBLE,
+    total_oral       DOUBLE,
+    rang_oral        INTEGER,
     FOREIGN KEY (candidat_id) REFERENCES Candidat (candidat_id)
 );
 
 CREATE TABLE Etablissement(
-    Etablissement text PRIMARY KEY,
-    Type          text,
-    Ville         text,
-    CP            int,
-    Pays          text,
+    Etablissement TEXT PRIMARY KEY,
+    Type          TEXT,
+    Ville         TEXT,
+    CP            INTEGER,
+    Pays          TEXT,
     FOREIGN KEY (Etablissement) REFERENCES Candidat (Etablissement)
 );
 
 CREATE TABLE Ecole(
-    ecole_id int PRIMARY KEY,
-    nom      text,
+    ecole_id INTEGER PRIMARY KEY,
+    nom      TEXT,
     FOREIGN KEY (ecole_id) REFERENCES Voeux (ecole_id)
 );
 
 CREATE TABLE Voeux(
-    candidat_id int,
-    ecole_id    int,
-    ordre       int,
+    candidat_id INTEGER,
+    ecole_id    INTEGER,
+    ordre       INTEGER,
     PRIMARY KEY (candidat_id, ecole_id)
 );
 
 CREATE TABLE Admission_ATS(
-    candidat_id int PRIMARY KEY,
-    rang        int,
+    candidat_id INTEGER PRIMARY KEY,
+    rang        INTEGER,
     FOREIGN KEY (candidat_id) REFERENCES Candidat (candidat_id)
 );
 
 CREATE TABLE Admis_ATS(
-    candidat_id int PRIMARY KEY,
-    rang        int,
+    candidat_id INTEGER PRIMARY KEY,
+    rang        INTEGER,
     FOREIGN KEY (candidat_id) REFERENCES Candidat (candidat_id)
 );
 
 CREATE TABLE Admission_MP(
-    candidat_id int PRIMARY KEY,
-    rang        int,
+    candidat_id INTEGER PRIMARY KEY,
+    rang        INTEGER,
     FOREIGN KEY (candidat_id) REFERENCES Candidat (candidat_id)
 );
 
 CREATE TABLE Admis_MP(
-    candidat_id int PRIMARY KEY,
-    rang        int,
+    candidat_id INTEGER PRIMARY KEY,
+    rang        INTEGER,
     FOREIGN KEY (candidat_id) REFERENCES Candidat (candidat_id)
 );
 
 CREATE TABLE Admis_MP_SPE(
-    candidat_id int PRIMARY KEY,
-    rang        int,
+    candidat_id INTEGER PRIMARY KEY,
+    rang        INTEGER,
     FOREIGN KEY (candidat_id) REFERENCES Candidat (candidat_id)
 );
 
 CREATE TABLE Admission_PC(
-    candidat_id int PRIMARY KEY,
-    rang        int,
+    candidat_id INTEGER PRIMARY KEY,
+    rang        INTEGER,
     FOREIGN KEY (candidat_id) REFERENCES Candidat (candidat_id)
 );
 
 CREATE TABLE Admis_PC(
-    candidat_id int PRIMARY KEY,
-    rang        int,
+    candidat_id INTEGER PRIMARY KEY,
+    rang        INTEGER,
     FOREIGN KEY (candidat_id) REFERENCES Candidat (candidat_id)
 );
 
 CREATE TABLE Admis_PC_SPE(
-    candidat_id int PRIMARY KEY,
-    rang        int,
+    candidat_id INTEGER PRIMARY KEY,
+    rang        INTEGER,
     FOREIGN KEY (candidat_id) REFERENCES Candidat (candidat_id)
 );
 
 CREATE TABLE Admission_PSI(
-    candidat_id int PRIMARY KEY,
-    rang        int,
+    candidat_id INTEGER PRIMARY KEY,
+    rang        INTEGER,
     FOREIGN KEY (candidat_id) REFERENCES Candidat (candidat_id)
 );
 
 CREATE TABLE Admis_PSI(
-    candidat_id int PRIMARY KEY,
-    rang        int,
+    candidat_id INTEGER PRIMARY KEY,
+    rang        INTEGER,
     FOREIGN KEY (candidat_id) REFERENCES Candidat (candidat_id)
 );
 
 CREATE TABLE Admis_PSI_SPE(
-    candidat_id int PRIMARY KEY,
-    rang        int,
+    candidat_id INTEGER PRIMARY KEY,
+    rang        INTEGER,
     FOREIGN KEY (candidat_id) REFERENCES Candidat (candidat_id)
 );
 
 CREATE TABLE Admission_PT(
-    candidat_id int PRIMARY KEY,
-    rang        int,
+    candidat_id INTEGER PRIMARY KEY,
+    rang        INTEGER,
     FOREIGN KEY (candidat_id) REFERENCES Candidat (candidat_id)
 );
 
 CREATE TABLE Admis_PT(
-    candidat_id int PRIMARY KEY,
-    rang        int,
+    candidat_id INTEGER PRIMARY KEY,
+    rang        INTEGER,
     FOREIGN KEY (candidat_id) REFERENCES Candidat (candidat_id)
 );
 
 CREATE TABLE Admis_PT_SPE(
-    candidat_id int PRIMARY KEY,
-    rang        int,
+    candidat_id INTEGER PRIMARY KEY,
+    rang        INTEGER,
     FOREIGN KEY (candidat_id) REFERENCES Candidat (candidat_id)
 );
 
 CREATE TABLE Admission_TSI(
-    candidat_id int PRIMARY KEY,
-    rang        int,
+    candidat_id INTEGER PRIMARY KEY,
+    rang        INTEGER,
     FOREIGN KEY (candidat_id) REFERENCES Candidat (candidat_id)
 );
 
 CREATE TABLE Admis_TSI(
-    candidat_id int PRIMARY KEY,
-    rang        int,
+    candidat_id INTEGER PRIMARY KEY,
+    rang        INTEGER,
     FOREIGN KEY (candidat_id) REFERENCES Candidat (candidat_id)
 );
 
 CREATE TABLE Admis_TSI_SPE(
-    candidat_id int PRIMARY KEY,
-    rang        int,
+    candidat_id INTEGER PRIMARY KEY,
+    rang        INTEGER,
     FOREIGN KEY (candidat_id) REFERENCES Candidat (candidat_id)
 );
