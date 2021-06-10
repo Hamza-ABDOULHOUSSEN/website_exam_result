a78584512bb1d863d8fff7b7a3ae529d82e28d31
Lucas Gouin <Lucas.Gouin@telecomnancy.eu>
2021-06-10 21:07:34 +0200

    ajout de message d'avancement aux scripts

--- create_table.sh
+++ create_table.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 rm CMT_database.db
+source env/bin/activate
+echo "[+] Génération de la base de données"
 python3 Base\ de\ donnee/DB_generator.py
+echo "[+] Remplissage de la base de données"
 python3 Base\ de\ donnee/DB_insertion.py
+echo "[+] Ajout des cas particuliers"
 python3 Base\ de\ donnee/DB_insertion_particulier.py
+deactivate
