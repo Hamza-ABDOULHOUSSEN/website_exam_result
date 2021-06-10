01d84a3c1027f1898a92ec862f91d2665f10cc22
Lucas Gouin <Lucas.Gouin@telecomnancy.eu>
2021-06-10 21:10:16 +0200

    ajout [+] au messages

--- init.sh
+++ init.sh
@@ -3,6 +3,6 @@ echo "[+] Création de l'environnement virtuel"
 python3 -m venv env
 echo "[+] Ouverture de l'environnement virtuel"
 source env/bin/activate
-echo "Installation des dépendances"
+echo "[+] Installation des dépendances"
 pip3 install -r requirements.txt
 deactivate
